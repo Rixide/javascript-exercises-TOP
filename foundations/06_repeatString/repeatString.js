6bd5544ff6da20dd352db95ac35b592cd7669d8b
Tsimaholison <tsimaholisonricardo@gmail.com>
2026-06-16 15:03:25 +0300

    3-7 exercise in the js foundations done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    let results ="";
 
+    if(number < 0){
+        return "ERROR"
+    }
+    for(let i=0; i < number; i++){
+        results+=word;
+    }
+    return results;
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let reversed = "";
+    let number = (words.length) ;
+    for(let i=0; i< number; number--){
+        reversed += words[(number - 1)];
+    }
+    return reversed
 };
 
 // Do not edit below this line
