75eb4de439bc1b5ea5920412b5af70ee4c5a3191
Tsimaholison Ricardo <tsimaholisonricardo@gmail.com>
2026-07-02 05:46:45 +0300

    exo 8,9 done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+
+    const numberOfArgument = arguments.length;
+    let result = arguments[0];
+    if(numberOfArgument <= 1) return arguments[0];
+    for(let i=1; i < numberOfArgument; i++){
+        for(let j=1; j < numberOfArgument; j++){
+            result = result.filter((item) => item !== arguments[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    let result = 0;
 
+    if( !Number.isInteger(numberOne) || !Number.isInteger(numberTwo) || numberOne < 0 || numberTwo < 0) return "ERROR";
+    if(numberOne > numberTwo){
+        for(let i= numberTwo; i <= numberOne; i++){
+            result+=i;
+        }
+    }else if(numberTwo > numberOne){
+        for(let i= numberOne; i <=numberTwo ; i++){
+            result+=i;
+        }
+    }else{
+        result = numberOne;
+    }
+    return result;
 };
 
 // Do not edit below this line
